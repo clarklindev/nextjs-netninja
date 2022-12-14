4fcd2fcf1f0d10b55b9217777da5be11a8e27f77
swagfinger <swagfinger@gmail.com>
2022-12-15 03:22:47 +0800

    images & metadata

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,19 @@
+import Head from 'next/head';
 function About() {
   return (
-    <div>
-      <h1>about</h1>
-      <p>
-        Eiusmod sunt velit duis id adipisicing do quis ullamco proident occaecat
-        excepteur.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja list | about</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>about</h1>
+        <p>
+          Eiusmod sunt velit duis id adipisicing do quis ullamco proident
+          occaecat excepteur.
+        </p>
+      </div>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>helloworld</h1>
-      <p className={styles.text}>
-        Proident veniam incididunt dolore labore nostrud eu eiusmod pariatur
-        laboris id commodo voluptate.
-      </p>
-      <Link href="/ninjas" className={styles.btn}>
-        list ninjas
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja list | home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>helloworld</h1>
+        <p className={styles.text}>
+          Proident veniam incididunt dolore labore nostrud eu eiusmod pariatur
+          laboris id commodo voluptate.
+        </p>
+        <Link href="/ninjas" className={styles.btn}>
+          list ninjas
+        </Link>
+      </div>
+    </>
   );
 }
