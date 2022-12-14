09dc52a7ca62ee4eccfab61509d67855440caed3
swagfinger <swagfinger@gmail.com>
2022-12-15 02:02:03 +0800

    adding <Layout> component

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -1,21 +1,14 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>helloworld</h1>
       <p>
         Proident veniam incididunt dolore labore nostrud eu eiusmod pariatur
         laboris id commodo voluptate.
       </p>
       <Link href="/ninjas">list ninjas</Link>
-      <Footer />
     </div>
   );
 }
